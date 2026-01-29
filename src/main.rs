10daa29413b2aa80d1ff96031a6071937395c7a4
Pete Goodall <pete@digicratic.com>
2026-01-29 14:45:45 +0000

    First working version.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+fn starts_with_vowel(word: &str) -> bool {
+    let first_letter = word.chars().nth(0);
+    match first_letter {
+        Some(l) => {
+            match l { //Is there a more efficient way to do this?
+                'a' => return true,
+                'e' => return true,
+                'i' => return true,
+                'o' => return true,
+                'u' => return true,
+                _ => return false,
+            }
+        },
+        None => return false, // I have to return false, but this doesn't see useful.
+    }
+}
+
+fn translate_with_vowel(word: &str) {
+    println!("{word}-hay");
+}
+
+fn translate_with_consonant(word: &str) {
+    let first_letter = &word.chars().nth(0).unwrap().to_string();
+    let rest_of_word = &word[1..];
+    let pig_latin = rest_of_word.to_string() + "-" + first_letter + "ay";
+    println!("{pig_latin}");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let word_list: Vec<&str> = vec!["hobby", "apple", "mirror", "scrap", "eagle", "trolly", "otter", "umbrella", "vessel", "1"];
+    for word in &word_list {
+        if starts_with_vowel(word) {
+            translate_with_vowel(word);
+        } else {
+            translate_with_consonant(*word);
+        }   
+    }
 }
